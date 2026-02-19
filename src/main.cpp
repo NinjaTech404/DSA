6d8361e0411f1d7d4e29bc5c981d2dde49d03e72
NinjaTech404 <ninjatech404@gmail.com>
2026-02-19 11:47:15 +0300

    New CMake Configs, Project Structure Updated!

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
-//#include<DS/array/array.h>
-//#include<DS/vector/vector.h>
-//#include<DS/string/string.h>
+#include <iostream>
+int main(int argc, char *argv[]) {
+  std::cout << "Edit this file and include the libraries as structured in "
+               "CMakeLists.txt\n";
+  return 0;
+}
