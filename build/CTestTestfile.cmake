10a201e0d0a60aa42bde224b2ef7f2c5395f2f07
NinjaTech <ninjatech404@gmail.com>
2026-02-19 08:56:23 +0000

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/paradox/Projects/DSA
-# Build directory: /home/paradox/Projects/DSA/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[MAIN_DSA_TESTS]=] "/home/paradox/Projects/DSA/build/main")
-set_tests_properties([=[MAIN_DSA_TESTS]=] PROPERTIES  WORKING_DIRECTORY "/home/paradox/Projects/DSA/build" _BACKTRACE_TRIPLES "/home/paradox/Projects/DSA/CMakeLists.txt;44;add_test;/home/paradox/Projects/DSA/CMakeLists.txt;0;")
